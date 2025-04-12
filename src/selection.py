b444ca52934bf26a43d1e0cb820b9b74db1782f3
CTepedino <ctepedino@itba.edu.ar>
2025-04-12 02:56:24 -0300

    More selection methods

--- src/selection.py
+++ src/selection.py
@@ -67,11 +67,86 @@ def ranking(k: int, individuals: [Individual]) -> [Individual]:
     n = len(individuals)
     def pseudo_fitness(index):
         return (n-index-1)/n
-    
+
+    pseudo_fitness_sum = 0
+    for i in range(n):
+        pseudo_fitness_sum += pseudo_fitness(i)
+
+    def relative_pseudo_fitness(index):
+        return pseudo_fitness(index)/pseudo_fitness_sum
+
+    accumulated_fitness = []
+    accum = 0
+    for i in range(n):
+        accum += relative_pseudo_fitness(i)
+        accumulated_fitness.append(accum)
+
+    selected = []
+    for _ in range(k):
+        r = random.random()
+        for i, accum in enumerate(accumulated_fitness):
+            if r <= accum:
+                selected.append(individuals[i].copy())
+                break
+    return selected
+
+_temperature = 1
+_initial_temperature = _temperature
+_min_temperature = 0.5
+_generation = 0
+
+def set_initial_temperature(value):
+    global _temperature
+    global _initial_temperature
+    _temperature = value
+    _initial_temperature = value
+
+def set_min_temperature(value):
+    global _min_temperature
+    _min_temperature = value
+
+def evolve_temperature():
+    global _temperature
+    global _generation
+
+    _generation += 1
+    _temperature = _min_temperature + (_initial_temperature - _min_temperature)*math.exp(-_temperature)
 
 
-def boltzmann():
-    return 0 #TODO
+
+def boltzmann(k: int, individuals: [Individual]) -> [Individual]:
+    temperature = _temperature
+
+    n = len(individuals)
+
+    population_averages = sum(map(lambda i: math.exp(i.get_fitness()/temperature), individuals))/n
+    def pseudo_fitness(individual):
+        return math.exp(individual.get_fitness()/temperature)/population_averages
+
+    pseudo_fitness_sum = 0
+    for individual in individuals:
+        pseudo_fitness_sum += pseudo_fitness(individual)
+
+    def relative_pseudo_fitness(individual):
+        return pseudo_fitness(individual)/pseudo_fitness_sum
+
+    accumulated_fitness = []
+    accum = 0
+    for individual in individuals:
+        accum += relative_pseudo_fitness(individual)
+        accumulated_fitness.append(accum)
+
+
+    evolve_temperature()
+
+    selected = []
+    for _ in range(k):
+        r = random.random()
+        for i, accum in enumerate(accumulated_fitness):
+            if r <= accum:
+                selected.append(individuals[i].copy())
+                break
+    return selected
 
 def deterministic_tournament():
     return 0 #TODO

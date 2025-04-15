650adba3d83d19a0b0601eaffc5926096406cce6
Alberto <abendayan@itba.edu.ar>
2025-04-14 21:26:15 -0300

    graficos buenisimos

--- src/graphics.py
+++ src/graphics.py
@@ -8,9 +8,14 @@ with open("../config/config_results.json", "r") as config_file:
 
 results_dir = config["results_dir"]
 archivos = config["files"]
+zoom = config["zoom"]
 nombre_salida = config.get("output_filename", "grafico_fitness")
 letra_inicio = config.get("tag_start", "C")
 letra_fin = config.get("tag_end", "M")
+
+# Directorio de salida
+output_dir = "../results_graphics"
+os.makedirs(output_dir, exist_ok=True)  # Crea el directorio si no existe
 
 plt.figure(figsize=(10, 6))
 
@@ -34,11 +39,17 @@ for archivo in archivos:
 plt.title("Evolución del Fitness según tipo de Cruza")
 plt.xlabel("Generación")
 plt.ylabel("Mejor Fitness")
-plt.ylim(0,1)
+if not zoom:
+    plt.ylim(0, 1)
 plt.legend()
 plt.grid(True)
 plt.tight_layout()
 
-# Guardar gráfico
-plt.savefig(f"{nombre_salida}.png")
-print(f"Gráfico guardado como {nombre_salida}.png")
+# Guardar gráfico en el nuevo directorio
+if not zoom:
+    path_salida = os.path.join(output_dir, f"{nombre_salida}.png")
+else:
+    path_salida = os.path.join(output_dir, f"{nombre_salida}_zoom.png")
+
+plt.savefig(path_salida)
+print(f"Gráfico guardado como {path_salida}")
